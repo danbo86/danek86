84ea6cca2bb0a605d3772c58ed17d287dd37699f
Danbo86 <danbo86@wp.pl>
2023-11-10 23:07:48 +0100

    tabela5

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -15,44 +15,6 @@ export default function Blog() {
             <BlogList />
           </div>
         </div>
-
-        {/* <div className="blog-column">
-
-          <h3 className="title">Artykuły</h3>
-
-          <div className="blog-box">
-
-          
-            <div className="blog-content">
-              <Link href="/" className='linek'>
-                <div className="content">
-                  <div className="year"><BsCalendarEvent className='kalendarz'/>2017 - 2018</div>
-                  <h3>Master degree- uniwersity</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aliquid. Nobis, officia deleniti. Qui explicabo illum a. Quae, expedita! Quaerat.</p>
-                </div>
-              </Link>
-            </div>
-
-            <div className="blog-content">
-              <div className="content">
-                <div className="year"><BsCalendarEvent className='kalendarz'/>2018 - 2019</div>
-                <h3>Master degree- uniwersity</h3>
-                
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aliquid. Nobis, officia deleniti. Qui explicabo illum a. Quae, expedita! Quaerat.</p>
-              </div>
-            </div>
-
-            <div className="blog-content">
-              <div className="content">
-                <div className="year"><BsCalendarEvent className='kalendarz'/>2019 - 2020</div>
-                <h3>Master degree- uniwersity</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aliquid. Nobis, officia deleniti. Qui explicabo illum a. Quae, expedita! Quaerat.</p>
-              </div>
-            </div>
-
-          </div>
-
-        </div> */}
       </div>
     </section>
   )

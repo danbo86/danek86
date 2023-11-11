0fa830da429758711cf0681dc66d4df69c8504d5
Danbo86 <danbo86@wp.pl>
2023-11-12 00:21:05 +0100

    csv i inne

--- components/Tabela.jsx
+++ components/Tabela.jsx
@@ -1,4 +1,7 @@
 import { supabaseTransakcje } from "../app/api/supabaseClientTransakcje"
+const csv = require("csv-parser")
+import { createReadStream } from "node:fs"
+import Link from "next/link"
 
 const konkurs = async () => {
   try {
@@ -8,9 +11,50 @@ const konkurs = async () => {
     }
   } catch (error) {
     console.log(error)
-    // throw new Error(error.message);
   }
 }
+
+// Ścieżka do pliku CSV
+const csvFilePath = "./app/daneFF.csv"
+
+// Nazwa tabeli w Supabase, do której chcesz przesłać dane
+const tableName = "konkurs"
+
+// Funkcja do przesyłania danych do Supabase
+const uploadDataToSupabase = async () => {
+  try {
+    // Otwórz strumień do odczytu plik CSV
+    const stream = createReadStream(csvFilePath)
+      .pipe(csv())
+      .on("data", async (row) => {
+        // results.push(row)
+
+        // aktualizacja danych do tabeli w Supabase
+        const { data, error } = await supabaseTransakcje
+          .from(tableName)
+          .update({
+            stan_konta: row.stan_konta,
+            procent_all: row.procent_all,
+            equity: row.equity,
+            imie: row.imie,
+          })
+          .eq("id", row.id)
+        if (error) {
+          console.error("Error inserting data:", error.message)
+        } else {
+          console.log("Data inserted successfully:")
+        }
+      })
+      .on("end", () => {
+        console.log("CSV file successfully processed.")
+      })
+  } catch (error) {
+    console.error("Error uploading data to Supabase:", error.message)
+  }
+}
+
+// Wywołaj funkcję przesyłania danych do Supabase
+uploadDataToSupabase()
 
 export default async function Tabela() {
   const konkurs_dane = await konkurs()
@@ -21,28 +65,36 @@ export default async function Tabela() {
   // console.log(konkurs_dane)
 
   return (
-    <div className="wrapper">
-      <table>
-        <caption>Wyniki</caption>
-        <thead>
-          <tr>
-            <th>Nick</th>
-            <th>Procent</th>
-            <th>Balance</th>
-            <th>Equity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {konkurs_dane.map((item) => (
-            <tr key={item.id}>
-              <td data-cell="Nick">{item.imie}</td>
-              <td data-cell="Procent">{item.procent_all} %</td>
-              <td data-cell="Balance">{item.stan_konta} USD</td>
-              <td data-cell="Equity">{item.equity} USD</td>
+    <>
+      <div className="wrapper">
+        <table>
+          <caption>Wyniki</caption>
+          <thead>
+            <tr>
+              <th>Nick</th>
+              <th>Procent</th>
+              <th>Balance</th>
+              <th>Equity</th>
+              <th>Link</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {konkurs_dane.map((item) => (
+              <tr key={item.id}>
+                <td data-cell="Nick">{item.imie}</td>
+                <td data-cell="Procent">{item.procent_all} %</td>
+                <td data-cell="Balance">{item.stan_konta} USD</td>
+                <td data-cell="Equity">{item.equity} USD</td>
+                <td data-cell="Url">
+                  <Link className="link-url" target="_blank" href={item.url}>
+                    Forex Factory
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }

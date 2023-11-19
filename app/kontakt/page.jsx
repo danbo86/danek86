281d56c9985d96cdfbdc07ff2dcb7a2f2490a623
Danbo86 <danbo86@wp.pl>
2023-11-19 19:52:24 +0100

    kontaktQR

--- app/kontakt/page.jsx
+++ app/kontakt/page.jsx
@@ -18,98 +18,93 @@ export default function Kontakt() {
 
   return (
     <section className="contact" id="contact">
-      <h2 className="heading">
-        Kontakt <span>do mnie</span>
-      </h2>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h2 className="heading">
+            Kontakt <span>do mnie</span>
+          </h2>
+          <div className="input-box">
+            <div className="input-field">
+              <input
+                name="name"
+                type="text"
+                placeholder="Imię"
+                required
+                minLength={3}
+                maxLength={200}
+              />
+              <span className="focus"></span>
+            </div>
 
-      <form onSubmit={handleSubmit}>
-        <div className="input-box">
-          <div className="input-field">
-            <input
-              name="name"
-              type="text"
-              placeholder="Imię"
+            <div className="input-field">
+              <input
+                name="nazwisko"
+                type="text"
+                placeholder="Nazwisko"
+                required
+                minLength={3}
+                maxLength={200}
+              />
+              <span className="focus"></span>
+            </div>
+          </div>
+
+          <div className="input-box">
+            <div className="input-field">
+              <input name="email" type="email" placeholder="Email" required />
+              <span className="focus"></span>
+            </div>
+          </div>
+
+          <div className="textarea-field">
+            <textarea
+              name="textarea"
+              id="textarea"
+              cols="30"
+              rows="10"
+              placeholder="Twoja wiadomość"
               required
-              minLength={3}
-              maxLength={200}
-            />
+            ></textarea>
             <span className="focus"></span>
           </div>
 
-          <div className="input-field">
-            <input
-              name="nazwisko"
-              type="text"
-              placeholder="Nazwisko"
-              required
-              minLength={3}
-              maxLength={200}
-            />
-            <span className="focus"></span>
+          <div className="btn-box btns">
+            <button type="submit" className="btn">
+              Wyślij
+            </button>
           </div>
-        </div>
+        </form>
+      </div>
 
-        <div className="input-box">
-          <div className="input-field">
-            <input name="email" type="email" placeholder="Email" required />
-            <span className="focus"></span>
-          </div>
-
-          {/* <div className="input-field">
-            <input type="text" placeholder="email" required/>
-            <span className="focus"></span>
-          </div> */}
-        </div>
-
-        <div className="textarea-field">
-          <textarea
-            name="textarea"
-            id="textarea"
-            cols="30"
-            rows="10"
-            placeholder="Twoja wiadomość"
-            required
-          ></textarea>
-          <span className="focus"></span>
-        </div>
-
-        <div className="btn-box btns">
-          <button type="submit" className="btn">
-            Wyślij
-          </button>
-        </div>
-
-        <div className="kontakt-zello">
-          <p className="kontakt-h3">
-            Kontakt na zello: <br /> zeskanuj kod QR lub kliknij w ikone Zello
-          </p>
-          <br />
-          <div>
-            <Image
-              src={"/images/zello_qr.jpg"}
-              layout="responsive" // Ustawienie na "responsive" zapewni responsywność
-              width={100}
-              height={100}
-              objectFit="contain" // Dostosuj objectFit do swoich preferencji
-              alt="Zello QR Code"
-              className="qr-code"
-            />
-          </div>
-        </div>
-      </form>
-
-      <div className="zello-box">
-        <Link
-          href="https://zello.page/GovTUmp6d6sEJxU19"
-          className="link-zello"
-          target="_blank"
-        >
-          <img
-            src="/icons/zello.png"
-            alt="logo-zello"
-            className="ikona-zello"
+      <div className="kontakt-zello">
+        <p className="kontakt-h3">
+          Kontakt na zello: <br /> zeskanuj kod QR lub kliknij w ikone Zello
+        </p>
+        <br />
+        <div>
+          <Image
+            src={"/images/zello_qr.jpg"}
+            layout="responsive" // Ustawienie na "responsive" zapewni responsywność
+            width={100}
+            height={100}
+            objectFit="contain" // Dostosuj objectFit do swoich preferencji
+            alt="Zello QR Code"
+            className="qr-code"
           />
-        </Link>
+        </div>
+        <div className="zello-box">
+          <Link
+            href="https://zello.page/GovTUmp6d6sEJxU19"
+            className="link-zello"
+            target="_blank"
+          >
+            <img
+              src="/icons/zello.png"
+              alt="logo-zello"
+              className="ikona-zello"
+            />
+          </Link>
+        </div>
       </div>
     </section>
   )

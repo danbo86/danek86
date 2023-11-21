77e9e05e3dc6fd45e8f518b6a5ede3e416a80297
Danbo86 <danbo86@wp.pl>
2023-11-21 21:35:24 +0100

    tom tom

--- app/konkurs_Zello/Live.jsx
+++ app/konkurs_Zello/Live.jsx
@@ -10,7 +10,7 @@ const Tabela = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const rowNumber = [6, 17, 28, 34, 46, 57]
+        const rowNumber = [6, 17, 28, 34, 46, 57, 68]
 
         // const columnName = "4"
 

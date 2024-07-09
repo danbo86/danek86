f77e7bfd8944d45fba7d5078465dd178a914ec0a
Danbo86 <danbo86@wp.pl>
2024-07-09 13:46:51 +0200

    smerf maruda

--- app/konkurs_Zello/Live.jsx
+++ app/konkurs_Zello/Live.jsx
@@ -10,7 +10,7 @@ const Tabela = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const rowNumber = [6 ,17, 28, 34, 46, 57, 68, 73, 74]
+        const rowNumber = [6, 17, 28, 34, 46, 57, 68, 73, 74, 75]
 
         // const columnName = "4"
 
